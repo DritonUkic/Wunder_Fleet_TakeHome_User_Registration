2a277b54ee14ec93d39ed2b156c0cfd41cb85781
Driton Ukic <dritonukic@outlook.de>
2019-06-15 17:07:42 +0200

    Added Class

---
+++ src/app/models/customerData.ts
@@ -0,0 +1,8 @@
+import * as registerData from './register'
+
+export class customerData{
+    personalData: registerData.Personal;
+    addressData: registerData.Address;
+    paymentData: registerData.Payment;
+    lastRegisterStep: number;
+}
